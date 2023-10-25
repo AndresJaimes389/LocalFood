ff8dcaa39579d98b40180a6a7699a5e1281811e3
Andresjl389 <andresjaimes195@gmail.com>
2023-10-25 11:55:04 -0500

    cambios mínimos

--- Aplicaciones/Conexion/urls.py
+++ Aplicaciones/Conexion/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # path('add/',views.add_person),
     # path('show/',views.get_all_person),
     path('singin/', views.signin, name = 'signin'),
-    path('singup/', views.Hola, name = 'signup'),
     path('login/', views.login_view, name = 'login'),
     path('logout/', views.signout, name = 'logout'),
     # path('inicio/', views.inicio, name = 'inicio'),
